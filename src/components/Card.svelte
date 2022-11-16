ca79ea9c8152f9f8971deeb494de5dd39fb6f646
T-O-R-U-S <bageliq@protonmail.com>
2022-11-17 01:01:46 +0400

    Initial commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let subtitle = "";
+	export let href = "";
+	export let title = "Filler";
+</script>
+
+<div>
+	<a href={href}>
+		<h2>{title}</h2>
+		<p>{subtitle}</p>
+		<slot />
+	</a>	
+</div>
+
+<style>
+	div {
+		padding: 3rem;
+		margin: 2rem;
+		background-color: white;
+
+		outline-color: var(--accent);
+		outline-width: 5px;
+		outline-style: solid;
+		outline-offset: 0.5rem;
+		
+		border-radius: 1rem;
+
+		text-align: center;
+
+		min-width: 30rem;
+		width: min-content;
+	}
+</style>
---
+++ src/pages/carousel.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Card from '../components/Card.svelte';
+</script>
+
+<style>
+    nav {
+        display: flex;
+    }
+
+    nav {
+        width: 100%;
+        height: 20rem;
+        display: flex;
+        overflow-x: visible;
+
+        animation: 10s linear slide infinite;
+    }
+
+    @keyframes slide {
+  		100% {
+            translate: -100% 0;
+        }
+	}
+</style>
+
+<nav>
+    <Card subtitle="A language with a LISP-like syntax." href="#" title="Shrimp3">
+        <code>
+            <pre>
+@main() &lbrace;
+decl x [1 2 3 add(4 4)]
+x[3] -&gt; y
+println(x) # Outputs 8. #
+&rbrace;
+            </pre>
+        </code>
+    </Card>
+    <Card subtitle="A social media website with a sprinkle on top."  href="#" title="Donut Planet"        ></Card>
+    <Card subtitle="A fantasy console that runs on bytecode."        href="#" title="ATC Fantacy Console" ></Card>
+    <Card subtitle="A JavaScript preprocessor with syntactic sugar." href="#" title="KongScript"          ></Card>
+    <Card href="#" title="Ipsum"></Card>
+    <Card href="#" title="Dolor"></Card>
+</nav>
